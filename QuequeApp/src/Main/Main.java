2168bbbdacb73642e0b38b8502de293a951c8057
GGfpc <ggfpc12495@gmail.com>
2015-10-31 17:46:48 +0000

    Main

--- QuequeApp/src/Main/Main.java
+++ QuequeApp/src/Main/Main.java
@@ -1,5 +1,12 @@
 package Main;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import GUI.ControlerGUI;
+import GUI.Utilizador;
 import GUI.ViewGUI;
 
 public class Main {
@@ -7,6 +14,9 @@ public class Main {
 	public static void main(String[] args){
 		ViewGUI view = new ViewGUI();
 		view.open();
+		
+		
+		
 	}
 
 }
